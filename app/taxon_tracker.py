c6834428cf0f0ebad49e0f6c2f6398856f92558e
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2022-05-09 16:33:24 +0100

    Fix taxon_tracker workflow

--- app/taxon_tracker.py
+++ app/taxon_tracker.py
@@ -1,10 +1,14 @@
 import logging
 import os
 import pandas
+import datetime
 
+import pytz
 from sqlalchemy import create_engine
 from time import sleep
 from enum import Enum
+
+from backend import fetch_accession_numbers
 
 
 # Adapted from https://stackoverflow.com/a/67305494
@@ -98,7 +102,6 @@ def get_connection() -> object:
 def get_taxons_to_check() -> pandas.DataFrame:
     taxon_id = COLUMNS[Tables.TAXON].TAXON_ID.value
     last_updated = COLUMNS[Tables.TAXON].LAST_UPDATED.value
-    time_added = COLUMNS[Tables.TAXON].TIME_ADDED.value
     # Fetch accessions for outdated taxon_ids
     return pandas.read_sql(
         sql=(
@@ -122,7 +125,6 @@ def update_records(taxon_id: int) -> None:
     logger.debug(f"Updating records for taxon id {taxon_id}.")
     # Query ENA for all accession numbers
     all_accessions = query_ENA(taxon_id)
-    logger.debug(f"ENA has {len(all_accessions)} records.")
 
     # Strip out existing accession numbers
     accession_number = COLUMNS[Tables.ACCESSION].ACCESSION_NUMBER.value
@@ -134,65 +136,121 @@ def update_records(taxon_id: int) -> None:
         ),
         con=get_connection()
     )
-    new_records = [n for n in all_accessions if n not in local_records]
+
+    new_records = all_accessions.loc[~(all_accessions[accession_number].isin(local_records[accession_number]))]
 
     if len(new_records) == 0:
         logger.debug(f"All ENA {len(all_accessions)} records exist locally.")
-        return
+    else:
+        logger.debug((
+            f"ENA has {len(all_accessions)} records. "
+            f"{len(local_records)} local records found. "
+            f"Fetching {len(new_records)} new records."
+        ))
 
-    logger.debug((
-        f"ENA has {len(all_accessions)} records. "
-        f"{len(local_records)} local records found. "
-        f"Fetching {len(new_records)} new records."
-    ))
+        # Add new records to Accessions table
+        new_records[t_id] = [taxon_id for _ in range(len(new_records))]
+        # timezone (tz) should match web/settings/settings.py
+        new_records[COLUMNS[Tables.ACCESSION].TIME_ADDED.value] = \
+            [datetime.datetime.now(tz=pytz.UTC) for _ in range(len(new_records))]
+        new_records.to_sql(
+            name=Tables.ACCESSION.value,
+            con=get_connection(),
+            if_exists='append',
+            index=False,
+            method='multi'
+        )
 
-    # Add new records to Accessions table
-    pandas.DataFrame({t_id: taxon_id, accession_number: new_records})\
-        .to_sql(name=Tables.ACCESSION.value, con=get_connection(), index=False)
-
-    # Fetch detailed record for new accession numbers
-    records = query_ENA_detail(new_records)
-    records.to_sql(name=Tables.RECORD_DETAILS.value, con=get_connection(), index=False)
-    logger.info(f"Added {len(new_records)} new records for taxon id {taxon_id}.")
+        logger.info(f"Added {len(new_records)} new records for taxon id {taxon_id}.")
 
     # Filter new records for suitability
-    filter_accession_numbers(taxon_id)
+    filter_accession_numbers()
 
 
 def query_ENA(taxon_id: int) -> pandas.DataFrame:
     logger.debug(f"Fetching ENA accession numbers for taxon id {taxon_id}.")
-    return pandas.DataFrame()
+    accession_numbers = fetch_accession_numbers.fetch_records_direct(
+        taxon_id=str(taxon_id),
+        accession_type='run',
+        print_result=False
+    )
+    df = pandas.DataFrame(accession_numbers)
+    df[COLUMNS[Tables.ACCESSION].ACCESSION_NUMBER.value] = df[['accession']]  # rename from backend
+    return df[[COLUMNS[Tables.ACCESSION].ACCESSION_NUMBER.value]]
 
 
-def query_ENA_detail(accession_numbers: list) -> pandas.DataFrame:
-    logger.debug(f"Fetching new ENA record details.")
-    return pandas.DataFrame()
-
-
-def filter_accession_numbers(taxon_id: int) -> None:
+def filter_accession_numbers() -> None:
+    """
+    Fetch records for any accession numbers without a passed_filter decision and apply filters.
+    """
     accession_number = COLUMNS[Tables.ACCESSION].ACCESSION_NUMBER.value
-    t_id = COLUMNS[Tables.ACCESSION].TAXON_ID.value
+    accession_number_fk = COLUMNS[Tables.RECORD_DETAILS].ACCESSION_NUMBER.value
     passed_filter = COLUMNS[Tables.ACCESSION].PASSED_FILTER.value
     accessions = pandas.read_sql(
         sql=(
-            f"SELECT {accession_number, passed_filter} FROM {Tables.ACCESSION.value} WHERE "
-            f"{t_id} = {taxon_id} AND {passed_filter} is null"
+            f"SELECT {accession_number}, {passed_filter} FROM {Tables.ACCESSION.value} WHERE "
+            f"{passed_filter} is null"
         ),
         con=get_connection()
     )
+
+    if len(accessions) == 0:
+        return
+
+    # Fetch detailed records if they don't already exist.
+    # This happens as a subset of accession numbers needing filtration because we might want to
+    # delete the record details after completed filter application because the complete records
+    # are much larger than the accession number summary and are only needed for filtering.
+    missing = pandas.read_sql(
+        sql=(
+            f"SELECT {accession_number} FROM {Tables.ACCESSION.value} WHERE "
+            f"{passed_filter} is null AND "
+            f"{accession_number} not in (SELECT {accession_number_fk} FROM {Tables.RECORD_DETAILS.value})"
+        ),
+        con=get_connection()
+    )
+    records = query_ENA_detail(missing[accession_number])
+    records.to_sql(
+        name=Tables.RECORD_DETAILS.value,
+        con=get_connection(),
+        if_exists='append',
+        index=False,
+        method='multi'
+    )
+
+    # Check records against filters
     records = pandas.read_sql(
         sql=(
-            f"SELECT * FROM {Tables.RECORD_DETAILS} WHERE "
-            f"{COLUMNS[Tables.RECORD_DETAILS].ACCESSION_NUMBER.value} in "
-            f"({', '.join(accessions[accession_number])})"
+            f"SELECT * FROM {Tables.RECORD_DETAILS.value} WHERE "
+            f"{accession_number_fk} in "
+            f"{tuple(accessions[accession_number])}"
         ),
         con=get_connection()
     )
+    logger.debug('Records:')
+    logger.debug(records)
+
     records[passed_filter] = True
 
-    new_accessions = records.loc[:, [accession_number, passed_filter]]
-    new_accessions.to_sql(name=Tables.ACCESSION, con=get_connection(), index=False)
-    logger.info(f"{len(records.loc[records[passed_filter]])}/{len(records)} new records acceptable for assembly.")
+    # Save results
+    new_accessions = records[[accession_number_fk, passed_filter]]
+    new_accessions.to_sql(
+        name=Tables.ACCESSION.value,
+        con=get_connection(),
+        if_exists='append',
+        index=False,
+        method='multi'
+    )
+
+    if len(records) > 0:
+        logger.info(f"{len(records.loc[records[passed_filter]])}/{len(records)} new records acceptable for assembly.")
+
+
+def query_ENA_detail(accession_numbers: list) -> pandas.DataFrame:
+    logger.debug(f"Fetching {len(accession_numbers)} new ENA record details.")
+    df = pandas.DataFrame()
+    logger.debug(f"Fetched {len(df)}/{len(accession_numbers)} record details.")
+    return df
 
 
 if __name__ == '__main__':
@@ -203,7 +261,6 @@ if __name__ == '__main__':
         try:
             # Update taxon records
             taxon_ids = get_taxons_to_check()
-            logger.debug(taxon_ids)
             update_taxons(taxon_ids)
 
             # Check for available droplet

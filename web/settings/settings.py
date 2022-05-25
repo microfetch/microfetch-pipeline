50fe5e30a42ba7d10b1a67cea771831dc1be4dde
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2022-05-25 13:58:51 +0100

    localhost db

--- web/settings/settings.py
+++ web/settings/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('POSTGRES_PORT', 5432),
         'TEST': {
             'NAME': f"test_{os.environ.get('POSTGRES_DB', 'postgres')}"
